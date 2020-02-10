33171567b34a4c271a1142c55032b649fa995165
khenryjoe <joseph.ketterer@gmail.com>
2020-02-10 16:01:28 +0000

    styling

--- src/screens/IndexScreens.js
+++ src/screens/IndexScreens.js
@@ -1,7 +1,7 @@
 import React, { useContext } from "react";
-import { View, Text, StyleSheet, FlatList, Button } from "react-native";
+import { View, Text, StyleSheet, FlatList, Button, TouchableOpacity } from "react-native";
 import { Context } from '../context/BlogContext';
-
+import { Feather } from '@expo/vector-icons';
 
 const IndexScreen = () => {
   const { state, addBlogPost } = useContext(Context);
@@ -14,7 +14,12 @@ const IndexScreen = () => {
       <FlatList
         data={state}
         renderItem={({ item }) => {
-          return <Text>{item.title}</Text>;
+          return (
+            <TouchableOpacity style={styles.row}>
+            <Text style={styles.title}>{item.title}</Text>
+          <Feather style={styles.icon} name="trash" />
+          </TouchableOpacity>
+          )
         }}
         keyExtractor={item => item.title}
       />
@@ -24,4 +29,20 @@ const IndexScreen = () => {
 
 export default IndexScreen;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  row:{
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    paddingVertical: 20,
+    paddingHorizontal: 10,
+    borderTopWidth: 1,
+   
+    borderColor: 'gray'
+  },
+  title:{
+    fontSize: 18
+  },
+  icon: {
+    fontSize: 24
+  }
+});
